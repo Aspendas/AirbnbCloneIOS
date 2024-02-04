98d9db56c7ed121ef23d25b592d77d350960ec7f
Aspendas <umutyasin2001@gmail.com>
2024-02-04 12:31:13 +0300

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Umut Y. Çalışkan on 4.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
