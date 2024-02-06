6962632e27b148abbf017a82a58a79188929be11
Aspendas <umutyasin2001@gmail.com>
2024-02-06 23:09:41 +0300

    add dummy amenities listing

--- AirbnbClone/Core/Listings/ListingDetailView.swift
+++ AirbnbClone/Core/Listings/ListingDetailView.swift
@@ -136,8 +136,30 @@ struct ListingDetailView: View {
             
             Divider()
             
-            //
-            VStack(spacing: 16) {
+            //listing amenities
+            VStack(alignment:.leading, spacing: 16) {
+                Text("What this place offers")
+                    .font(.headline)
+                
+                ForEach(0 ..< 5) {
+                    feature in HStack {
+                        Image(systemName: "wifi")
+                            .frame(width: 32)
+                        
+                        Text("Wifi")
+                            .font(.footnote)
+                        
+                        Spacer()
+                        
+                    }
+                }
+            }
+            .padding()
+            
+            Divider()
+            
+            //Map View
+            VStack{
                 
             }
         }
