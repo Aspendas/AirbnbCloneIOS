a42d05e07c34b62502fad232c480aaa3731093fc
Aspendas <umutyasin2001@gmail.com>
2024-02-08 18:56:42 +0300

    add basic destination search view

--- AirbnbClone/Core/Explore/SearchAndFilterBar.swift
+++ AirbnbClone/Core/Explore/SearchAndFilterBar.swift
@@ -24,10 +24,12 @@ struct SearchAndFilterBar: View {
             
                 Spacer()
                 
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            Button{
+                
+            } label: {
                     Image(systemName: "line.3.horizontal.decrease.circle")
                         .foregroundStyle(.black)
-                })
+                }
             }
             .padding(.horizontal)
             .padding(.vertical, 10)
